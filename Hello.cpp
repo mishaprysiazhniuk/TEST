4221cd5efc17977a22372205d94f95aa2b698d89
mishaprysiazhniuk <mishaprisyajnyuk@knu.ua>
2022-11-07 00:00:52 +0200

    first modification

---
+++ Hello.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout << "Number a:";
+    cin >> a;
+    cout << "Number b:";
+    cin >> b;
+    int c;
+    c = (a *a) + (2 * a * b) + (b*b);
+    cout << "It's c = " << c << endl;
+    float d;
+    
+    d = sqrt(((a*a ) + (b *b))) ;
+    cout << "It's Pifagooorrorrr:" << d;
+}
